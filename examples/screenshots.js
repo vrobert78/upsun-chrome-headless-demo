cb5dd9a8ac9ffa610c4b49d19807c605b01d2361
chadcarlson <chad.carlson@platform.sh>
2019-05-30 18:02:04 -0400

    update launch syntax

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -9,9 +9,16 @@ const credentials = config.credentials('chrome');
 const puppeteer = require('puppeteer');
 
 (async() => {
-  const browser = await puppeteer.launch(credentials.host);
+
+  const browser = await puppeteer.launch({
+    executablePath: credentials.host
+  });
+
+
   const page = await browser.newPage();
   await page.goto('http://platform.sh');
   await page.screenshot({path: 'screenshots/example.png'});
   await browser.close();
 })();
+
+
