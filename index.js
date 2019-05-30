97419e8165d73566dbc1945647904a44dc8d795d
chadcarlson <chad.carlson@platform.sh>
2019-05-30 11:31:33 -0400

    delete additional vestigials

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const credentials = config.credentials('chrome');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
+  response.end("<html><head><title>Hello node</title></head><body><h1>Headless Chrome on Platform.sh</h1><h3>Relationship:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
 });
 
 server.listen(config.port);
