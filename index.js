160286c7049100c2d0ceac64ad90aec37680835a
chadcarlson <chad.carlson@platform.sh>
2019-05-30 11:06:38 -0400

    don't call chrome at all

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const platformsh = require("platformsh-config")
 
 let config = platformsh.config();
 
-const credentials = config.credentials('chrome');
+//const credentials = config.credentials('chrome');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
+  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
 });
 
 //var server = http.createServer(function (request, response) {
