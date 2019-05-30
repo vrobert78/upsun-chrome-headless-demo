13d8764a825fbfb1a1aafe3468313b8f57764d7a
chadcarlson <chad.carlson@platform.sh>
2019-05-30 17:49:47 -0400

    define executable path

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -1,9 +1,15 @@
 'use strict';
 
+const platformsh = require('platformsh-config');
+
+let config = platformsh.config();
+
+const credentials = config.credentials('chrome');
+
 const puppeteer = require('puppeteer');
 
 (async() => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch(credentials.host);
   const page = await browser.newPage();
   await page.goto('http://platform.sh');
   await page.screenshot({path: 'screenshots/example.png'});
