8bbf72b0fbb5e6107036c28e1d59e53f903ad948
chadcarlson <chad.carlson@platform.sh>
2019-05-30 11:25:20 -0400

    try adding chrome again

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const platformsh = require("platformsh-config")
 
 let config = platformsh.config();
 
-//const credentials = config.credentials('chrome');
+const credentials = config.credentials('chrome');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
+  response.end("<html><head><title>Hello node</title></head><body><h1>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
 });
 
 server.listen(config.port);
