811913adcae5d8ac0f8c80d3673a8ab00eaa11d5
chadcarlson <chad.carlson@platform.sh>
2019-05-30 10:45:03 -0400

    revert to original hello page

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var http = require('http');
 
 // Load the Platform.sh configuration.
 var config = require("platformsh").config();
-var credentials = config.credentials('chrome');
-
-//var server = http.createServer(function (request, response) {
-//  response.writeHead(200, {"Content-Type": "text/html"});
-//  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Node</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
-//});
+//var credentials = config.credentials('chrome');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
+  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
 });
+
+//var server = http.createServer(function (request, response) {
+//  response.writeHead(200, {"Content-Type": "text/html"});
+//  response.end("<html><head><title>Hello node</title></head><body><h1>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
+//});
 
 server.listen(config.port);
