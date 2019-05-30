8072348119849b22b817b53c00342997bd36e4c4
chadcarlson <chad.carlson@platform.sh>
2019-05-30 10:35:50 -0400

    left two server calls

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var http = require('http');
 var config = require("platformsh").config();
 var credentials = config.credentials('chrome');
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Node</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
-});
+//var server = http.createServer(function (request, response) {
+//  response.writeHead(200, {"Content-Type": "text/html"});
+//  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Node</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
+//});
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
