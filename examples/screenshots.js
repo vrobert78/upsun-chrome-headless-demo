a619baa9b548606cd8f4e522dac6bfa562e98af6
chadcarlson <chad.carlson@platform.sh>
2019-05-30 18:49:31 -0400

    proxy flag

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -10,9 +10,21 @@ const puppeteer = require('puppeteer');
 
 (async() => {
 
-  const browser = await puppeteer.connect({
-    browserURL: "http://0.0.0.0:" + credentials.port
+  const browser = await puppeteer.launch({
+    // Launch chromium using a proxy server on port 9876.
+    // More on proxying:
+    //    https://www.chromium.org/developers/design-documents/network-settings
+    args: [
+      '--proxy-server=' + credentials.ip + ':' + credentials.port,
+      // Use proxy for localhost URLs
+      '--proxy-bypass-list=<-loopback>',
+    ]
   });
+
+
+//  const browser = await puppeteer.connect({
+//    browserURL: "http://" + credentials.host + ":" + credentials.port
+//  });
 
 //  const browser = await puppeteer.launch();
   const page = await browser.newPage();
