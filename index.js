323e8647aa962feccf6c991f7c5a82b9011cba90
chadcarlson <chad.carlson@platform.sh>
2019-05-30 10:57:18 -0400

    updates

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var http = require('http');
 //var config = require("platformsh").config();
 //var credentials = config.credentials('chrome');
 
-const config = require("platformsh-config").config();
+const platformsh = require("platformsh-config")
+
+let config = platformsh.config();
 
 const credentials = config.credentials('chrome');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
+  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
 });
 
 //var server = http.createServer(function (request, response) {
