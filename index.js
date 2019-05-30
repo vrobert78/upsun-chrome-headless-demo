399529e5c5f9165194096982c17296b1296aefa5
chadcarlson <chad.carlson@platform.sh>
2019-05-30 13:10:46 -0400

    try to write instead of send

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ var express = require('express')
 var app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World! <a href="/test">Test page</a>')
+  res.writeHead(200, {"Content-Type": "text/html"});
+//  res.send('Hello World! <a href="/test">Test page</a>')
+  res.write(`<html>
+<head>
+    <title>Platform.sh Headless Chrome usage examples</title>
+</head>
+<body>
+<h1>Usage examples for Headless Chrome</h1>
+<ul>
+  <li><a href="/test">Test page</a></li>
+  <li><a href="/test">Test page</a></li>
+  <li><a href="/test">Test page</a></li>
+</ul>
+`);
+    res.end(`</body></html>`);
 })
 
 app.get('/test', (req, res) => {
