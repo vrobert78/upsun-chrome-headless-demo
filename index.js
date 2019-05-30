4f735370aa99fa9041cdc595f9ba608caca358f9
chadcarlson <chad.carlson@platform.sh>
2019-05-30 11:15:17 -0400

    delete vistigials

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // Load the http module to create an http server.
 var http = require('http');
 
-// Load the Platform.sh configuration.
-//var config = require("platformsh").config();
-//var credentials = config.credentials('chrome');
-
 const platformsh = require("platformsh-config")
 
 let config = platformsh.config();
@@ -13,12 +9,7 @@ let config = platformsh.config();
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
+  response.end("<html><head><title>Hello node</title></head><body><h1>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
 });
-
-//var server = http.createServer(function (request, response) {
-//  response.writeHead(200, {"Content-Type": "text/html"});
-//  response.end("<html><head><title>Hello node</title></head><body><h1>Hello Headless Chrome</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(credentials, null, 4) + "</pre></body></html>");
-//});
 
 server.listen(config.port);
