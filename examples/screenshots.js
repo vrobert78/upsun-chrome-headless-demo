62f1faac9e97f139da2f4c6803d3b04c384ae585
chadcarlson <chad.carlson@platform.sh>
2019-05-30 19:00:28 -0400

    revert

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -1,30 +1,17 @@
 'use strict';
 
-const platformsh = require('platformsh-config');
 
+const puppeteer = require('puppeteer');
+const platformsh = require('platformsh-config');
 let config = platformsh.config();
 
 const credentials = config.credentials('chrome');
 
-const puppeteer = require('puppeteer');
-
 (async() => {
 
-  const browser = await puppeteer.launch({
-    // Launch chromium using a proxy server on port 9876.
-    // More on proxying:
-    //    https://www.chromium.org/developers/design-documents/network-settings
-    args: [
-      '--proxy-server=' + credentials.ip + ':' + credentials.port,
-      // Use proxy for localhost URLs
-      '--proxy-bypass-list=<-loopback>',
-    ]
+  const browser = await puppeteer.connect({
+    browserURL: "http://" + credentials.host + ":" + credentials.port
   });
-
-
-//  const browser = await puppeteer.connect({
-//    browserURL: "http://" + credentials.host + ":" + credentials.port
-//  });
 
 //  const browser = await puppeteer.launch();
   const page = await browser.newPage();
