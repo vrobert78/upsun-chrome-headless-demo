a21488e393905dc315099e74b462ac031e77019f
chadcarlson <chad.carlson@platform.sh>
2019-05-30 21:49:59 -0400

    switch port

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -10,7 +10,7 @@ const credentials = config.credentials('chrome');
 (async() => {
 
   const browser = await puppeteer.connect({
-    browserURL: "http://" + credentials.host + ":" + credentials.port
+    browserURL: "http://" + credentials.host + ":9222"
   });
 
 //  const browser = await puppeteer.launch();
