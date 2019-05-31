f1948f8d171b955019fb4a678560a12eb62f8a0a
chadcarlson <chad.carlson@platform.sh>
2019-05-31 14:51:13 -0400

    add pdf script

--- screenshots.js
+++ screenshots.js
@@ -5,7 +5,7 @@ const platformsh = require('platformsh-config');
 let config = platformsh.config();
 const credentials = config.credentials('headless');
 
-const takeScreenshot = async function () {
+const getBrowser = async function () {
 
     try {
 
@@ -13,14 +13,10 @@ const takeScreenshot = async function () {
         const browser = await puppeteer.connect({browserURL: browserURL});
 
         const page = await browser.newPage();
-        await page.goto('https://platform.sh', {waitUntil: 'networkidle2'});
-        await page.pdf({
-            path: 'pdfs/example.pdf',
-            format: 'letter'
-        });
-        await browser.close();
+        await page.goto('https://google.com');
+        await page.screenshot({path: 'screenshots/example.png'});
 
-        return browser
+        await browser.close();
 
     } catch (e) {
 
@@ -29,3 +25,5 @@ const takeScreenshot = async function () {
     }
 
 };
+
+getBrowser();
