846c0e15c900c5514f98579bf79925af6712bed8
chadcarlson <chad.carlson@platform.sh>
2019-05-31 09:50:53 -0400

    update to endpoint

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -10,7 +10,8 @@ const credentials = config.credentials('chrome');
 (async() => {
 
   const browser = await puppeteer.connect({
-    browserURL: "http://" + credentials.host + ":" + credentials.port
+    browserWSEndpoint: "ws://169.254.199.59:9222/devtools/browser/e8d1f39a-c94f-4337-b585-8721337a080a",
+//    browserURL: "http://" + credentials.host + ":" + credentials.port
   });
 
   const page = await browser.newPage();
