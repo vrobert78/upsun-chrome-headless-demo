2bcf0148de096c3c9dd2e36be2de0e05ea63e027
chadcarlson <chad.carlson@platform.sh>
2019-05-31 14:46:04 -0400

    revert

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -5,18 +5,18 @@ const platformsh = require('platformsh-config');
 let config = platformsh.config();
 const credentials = config.credentials('headless');
 
-const getBrowser = async function () {
+const takeScreenshot = async function () {
 
     try {
 
         const browserURL = 'http://' + credentials.ip + ':9222';
         const browser = await puppeteer.connect({browserURL: browserURL});
 
-//        const page = await browser.newPage();
-//        await page.goto('https://google.com');
-//        await page.screenshot({path: 'screenshots/example.png'});
-//
-//        await browser.close();
+        const page = await browser.newPage();
+        await page.goto('https://platform.sh');
+        await page.screenshot({path: 'screenshots/example.png'});
+
+        await browser.close();
 
         return browser
 
@@ -29,26 +29,26 @@ const getBrowser = async function () {
 };
 
 
-const takeScreenshot = async function () {
-
-    try {
-
-        const browser = getBrowser();
-
-        const page = await browser.newPage();
-        await page.goto('https://google.com');
-        await page.screenshot({path: 'screenshots/example.png'});
-
-        await browser.close();
-
-
-    } catch (e) {
-
-        return Promise.reject(e);
-
-    }
-
-}
+//const takeScreenshot = async function () {
+//
+//    try {
+//
+//        const browser = getBrowser();
+//
+//        const page = await browser.newPage();
+//        await page.goto('https://google.com');
+//        await page.screenshot({path: 'screenshots/example.png'});
+//
+//        await browser.close();
+//
+//
+//    } catch (e) {
+//
+//        return Promise.reject(e);
+//
+//    }
+//
+//}
 
 //getBrowser();
 takeScreenshot();
