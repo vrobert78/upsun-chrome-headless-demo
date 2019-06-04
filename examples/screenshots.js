b15f4369b2344e2d50e006a1bcb6575e10ba413e
chadcarlson <chad.carlson@platform.sh>
2019-06-04 11:34:44 -0400

    register puppeteer formatter test

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -4,14 +4,24 @@ const platformsh = require('platformsh-config');
 let config = platformsh.config();
 const credentials = config.credentials('headless');
 
+
+function puppeteerFormatter(credentials) {
+	return `http://${credentials["ip"]}:${credentials["port"]}`;
+}
+
+// Call this in setup.
+config.registerFormatter("headless", puppeteerFormatter);
+
+
 var exports = module.exports = {};
 
 exports.takeScreenshot = async function (url, screenshotID) {
 
     try {
 //        const browserURL = 'http://' + credentials.ip + ':9222';
-        const browserURL = `http://${credentials.ip}:${credentials.port}`;
-        const browser = await puppeteer.connect({browserURL: browserURL});
+//        const browserURL = `http://${credentials.ip}:${credentials.port}`;
+        const formattedURL = config.FormattedCredentials("headless", "puppeteer");
+        const browser = await puppeteer.connect({browserURL: formattedURL});
 
         const page = await browser.newPage();
         await page.goto(url);
