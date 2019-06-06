ea71327047de775d0ad394bb71b24befdaca5647
chadcarlson <chad.carlson@platform.sh>
2019-06-06 10:05:31 -0400

    add emulate check

--- examples/screenshots.js
+++ examples/screenshots.js
@@ -8,7 +8,7 @@ const platformsh = require('platformsh-config');
 var exports = module.exports = {};
 
 // Create an async function
-exports.takeScreenshot = async function (url, screenshotID) {
+exports.takeScreenshot = async function (url, screenshotID, emulateMobile=false) {
 
     try {
         // Connect to chrome-headless using pre-formatted puppeteer credentials
@@ -18,6 +18,11 @@ exports.takeScreenshot = async function (url, screenshotID) {
 
         // Open a new page to the given url and take the screenshot
         const page = await browser.newPage();
+
+        if (emulateMobile) {
+            await page.emulate(devices['iPhone 6']);
+        }
+
         await page.goto(url);
         await page.screenshot({
             fullPage: true,
